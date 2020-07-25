b37b11e64268c90423b8fc2c20c1107835e748f0
huanglei <huanglei@quanjingshuju.com>
2020-07-26 07:16:42 +0800

    增加数据Model

---
+++ app/Models/Product.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Product extends Model
+{
+    use SoftDeletes;
+
+    /**
+     * 与模型关联的数据表
+     *
+     * @var string
+     */
+    protected $table = 'products';
+
+    /**
+     * 表主键
+     *
+     * @var string
+     */
+    protected $primaryKey = 'product_id';
+
+    /**
+     * 模型的日期字段保存格式。
+     *
+     * @var string
+     */
+    protected $dateFormat = 'U';
+
+    /**
+     * 被禁止修改的字段
+     *
+     * @var string[]
+     */
+    protected $guarded = ['created_at', 'updated_at', 'deleted_at'];
+}
