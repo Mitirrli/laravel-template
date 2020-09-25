813c1e69de526d86cf8a0db595fe82fa1ee3f895
mitirrli <phper.blue@gmail.com>
2020-09-25 15:31:18 +0800

    Add common return.

---
+++ app/Http/Middleware/ApiMiddleware.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Tools\RT;
+use Closure;
+use Illuminate\Http\Request;
+
+class ApiMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @param \Closure $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        $next($request);
+
+        return response()->json(RT()->getResult());
+    }
+}
