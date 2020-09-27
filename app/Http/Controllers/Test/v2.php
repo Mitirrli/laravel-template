bc5fd2b08c1a51cbbe77aed366def9898dc09e6f
mitirrli <phper.blue@gmail.com>
2020-09-27 09:35:49 +0800

    Add domain.

--- app/Http/Controllers/Test/v2.php
+++ app/Http/Controllers/Test/v2.php
@@ -8,7 +8,7 @@ use Collective\Annotations\Routing\Annotations\Annotations\Get;
 trait v2
 {
     /**
-     * @Get("/v2/test", as="测试接口v2")
+     * @Get("/v2/test", as="测试接口v2", domain="http://la.template.local.cc")
      */
     public function testV2(TestRequest $request)
     {
--- app/Http/Controllers/Test/v3.php
+++ app/Http/Controllers/Test/v3.php
@@ -7,7 +7,7 @@ use Collective\Annotations\Routing\Annotations\Annotations\Get;
 trait v3
 {
     /**
-     * @Get("/v3/test", as="测试接口v3")
+     * @Get("/v3/test", as="测试接口v3", domain="http://la.template.local.cc")
      */
     public function testV3()
     {
