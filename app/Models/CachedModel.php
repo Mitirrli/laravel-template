d1afd766a20fc887ca3f3afc878adf6217a978c3
mitirrli <phper.blue@gmail.com>
2020-09-29 16:12:45 +0800

    Add some common attributes.

---
+++ app/Models/CachedModel.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Models;
+
+use DateTimeInterface;
+
+class CachedModel extends \GeneaLabs\LaravelModelCaching\CachedModel
+{
+    const CREATED_AT = 'create_time';
+
+    const UPDATED_AT = 'update_time';
+
+    const DELETED_AT = 'delete_time';
+
+    protected $dateFormat = 'U';
+
+    protected $fillable = [];
+
+    protected $hidden = ['delete_time'];
+
+    /**
+     * 为数组 / JSON 序列化准备日期。
+     *
+     * @param DateTimeInterface $date
+     * @return string
+     */
+    protected function serializeDate(DateTimeInterface $date)
+    {
+        return $date->format($this->dateFormat ?: 'Y-m-d H:i:s');
+    }
+
+    /**
+     * 图片 字符串转数组。
+     *
+     * @param $value
+     * @return false|string[]
+     */
+    public function getPicturesAttribute($value)
+    {
+        return explode(',', $value);
+    }
+}
