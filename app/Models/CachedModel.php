d8a117a639adea32e20bbe00ce01fd7e29d16102
mitirrli <phper.blue@gmail.com>
2020-09-30 17:35:00 +0800

    Cs fix.

--- app/Models/CachedModel.php
+++ app/Models/CachedModel.php
@@ -6,6 +6,16 @@ namespace App\Models;
 
 use DateTimeInterface;
 
+/**
+ * App\Models\CachedModel.
+ *
+ * @method static \Illuminate\Database\Eloquent\Builder|CachedModel disableCache()
+ * @method static \GeneaLabs\LaravelModelCaching\CachedBuilder|CachedModel newModelQuery()
+ * @method static \GeneaLabs\LaravelModelCaching\CachedBuilder|CachedModel newQuery()
+ * @method static \GeneaLabs\LaravelModelCaching\CachedBuilder|CachedModel query()
+ * @method static \Illuminate\Database\Eloquent\Builder|CachedModel withCacheCooldownSeconds($seconds = null)
+ * @mixin \Eloquent
+ */
 class CachedModel extends \GeneaLabs\LaravelModelCaching\CachedModel
 {
     const CREATED_AT = 'create_time';
