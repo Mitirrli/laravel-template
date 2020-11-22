9e5445394b74dbe779250d949617c01eb07d46db
mitirrli <phper.blue@gmail.com>
2020-11-22 22:38:45 +0800

    Add render.

--- app/Exceptions/SystemException.php
+++ app/Exceptions/SystemException.php
@@ -4,6 +4,17 @@ declare(strict_types=1);
 
 namespace App\Exceptions;
 
+/**
+ * 系统异常
+ */
 class SystemException extends \Exception
 {
+    public function render()
+    {
+        return response()->json([
+            'code' => 67,
+            'msg' => '服务器开小差了, 请稍后再试~',
+            'data' => []
+        ]);
+    }
 }
